9b766eb0897b23ca25459d0d821ef5f99d08b66f
f1kart <kcrmotorsports@gmail.com>
2024-02-22 21:31:50 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dyconcrete.db'  # or 'postgresql://user:password@localhost/dyconcrete' for production
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///dytime.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
